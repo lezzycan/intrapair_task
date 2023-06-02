2139fa5fb05c76aea5a056deef387fc3b2c61ccd
Lezzycan <waheedolalekan23@example.com>
2023-06-02 01:52:31 +0100

    customer screen implemented

--- lib/ui/views/customer/customer_view.dart
+++ lib/ui/views/customer/customer_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intrapair_task/ui/common/app_colors.dart';
+import 'package:intrapair_task/ui/shared/app_spacing.dart';
+import 'package:intrapair_task/ui/shared/dumb_widgets/app_button.dart';
 import 'package:intrapair_task/ui/shared/dumb_widgets/app_textfield.dart';
 import 'package:stacked/stacked.dart';
 
@@ -19,6 +21,7 @@ class CustomerView extends StackedView<CustomerViewModel> {
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           leading: IconButton(
             onPressed: () => Navigator.pop(context),
@@ -33,29 +36,82 @@ class CustomerView extends StackedView<CustomerViewModel> {
           leadingWidth: 35.0,
           elevation: 0,
           bottom: TabBar(
-            labelColor: kcPrimaryColor,
-            unselectedLabelColor:kcLightGrey ,
-            indicatorColor: kcPrimaryColor,
-            indicatorPadding: const EdgeInsets.symmetric(horizontal: 18.0),
-            tabs: [
-            Tab(
-              text: 'Personal Data'.toUpperCase(),
-            ),
-            Tab(
-              text: 'Measurement info'.toUpperCase(),
-            )
-          ]),
+              labelColor: kcPrimaryColor,
+              unselectedLabelColor: kcLightGrey,
+              indicatorColor: kcPrimaryColor,
+              indicatorPadding: const EdgeInsets.symmetric(horizontal: 18.0),
+              tabs: [
+                Tab(
+                  text: 'Personal Data'.toUpperCase(),
+                ),
+                Tab(
+                  text: 'Measurement info'.toUpperCase(),
+                )
+              ]),
         ),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 20.h),
-          child:  TabBarView(children: [
-            AppTextFieldWidget(
-              labelText: const Text('Full name'),
-       
-              helperText: 'Assistive text',
-              borderRadius: BorderRadius.circular(8.0),
+          child: TabBarView(children: [
+            Column(
+              children: [
+                const AppSpacing.tinyHeight(),
+                AppTextFieldWidget(
+                  labelText: const Text('Full name'),
+                  helperText: 'Assistive text',
+                  borderRadius: BorderRadius.circular(8.0),
+                  labelStyle: _textTheme.bodySmall,
+                  
+                ),
+                AppSpacing.height(20.h),
+                     Align(
+                      alignment: Alignment.topLeft,
+                      child: Text('Gender', style: _textTheme.bodySmall!.copyWith(color: kcTextColor, fontSize: 12.sp),)),    
+                   Container(
+                    width: double.infinity,
+                    height: 50.h,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.r),
+                      border: Border.all(width: 1.0),
+
+                    ),
+                    child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      isExpanded: true,
+                      items: viewModel.genders.map((String gender) {
+                        return DropdownMenuItem<String>(
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(gender),
+                          ),
+                          value: gender,
+                        );
+                      }).toList(),
+                      onChanged: viewModel.onChangedGender,
+                      value: viewModel.selectedGender,
+                    //  icon: null,
+                    ),
+                  ),
+                 ),
+                      
+                AppSpacing.height(20.h),
+                AppTextFieldWidget(
+                  labelText:  const Text('Email Adress'),
+                  hintText: 'adeyinka@gmail.com',
+                   labelStyle: _textTheme.bodySmall!,
+                ),
+                    AppSpacing.height(20.h),
+                       AppTextFieldWidget(
+                  labelText:  const Text('Phone Number'),
+                  hintText: '+234 9036548321',
+                  hintTextStyle: _textTheme.bodySmall,
+                   labelStyle: _textTheme.bodySmall!,
+                ),
+                 AppSpacing.height(30.h),
+                AppButton(onPressed: (){}, label: 'Next')
+              ],
             ),
-            Text('leaf')
+            const Text('Hello Intrapair')
           ]),
         ),
       ),
--- lib/ui/views/customer/customer_viewmodel.dart
+++ lib/ui/views/customer/customer_viewmodel.dart
@@ -1,3 +1,21 @@
+import 'package:intrapair_task/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
 
-class CustomerViewModel extends BaseViewModel {}
+class CustomerViewModel extends BaseViewModel {
+  final _navigationService = locator<NavigationService>();
+
+  final List<String> _genders = ['. Not Specified', '. Male', '. FeMale'];
+
+  List<String> get genders => _genders;
+  String selectedGender = '. Not Specified';
+
+  void onChangedGender(String? value) {
+    selectedGender = value!;
+    notifyListeners();
+  }
+
+  void navigateBackToLogin() {
+    _navigationService.back();
+  }
+}
